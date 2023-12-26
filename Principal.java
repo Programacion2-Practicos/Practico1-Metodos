9c7de7065e833654defd2b0c35f6404dc5264e5f
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2023-12-26 19:30:42 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -4,8 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		//EJERCICIO 1//
-				Programa1 ejer1 = new Programa1();
-				ejer1.inicar();
+		Programa1 ejer1 = new Programa1();
+		ejer1.inicar();
 
 	}
 
