63ac45d084831a89f6035a9cc43b2bfdaa118fd2
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2023-12-26 20:39:17 -0300

    Update Programa.java

--- src/ejer3/Programa.java
+++ src/ejer3/Programa.java
@@ -15,65 +15,17 @@ public class Programa {
 	//carga de teclado dos String
 	String cargarString(){
 		
-		Scanner entrada=new Scanner(System.in);
-		 
-		 System.out.println("Ingrese un String: ");
-		 String oracion=entrada.nextLine();//para leer lineas de texto sino lee hasta un espacio en blanco
-		 return oracion;
+		//Complete aquÃ­
 		 
 		 
 	}
 	
-	//despliega menú
+	//despliega menÃº
 	void menu(String a, String b){
-		int select=-1;
-		char [] arr1;
-				
-		//Mientras la opción elegida sea 0, preguntamos al usuario
-			while(select != 0){
-					System.out.println("Menú de opciones\n"+
-							    "\n1.- Imprimir ambas cadenas y su largo." +
-								"\n2.- Comparar igualdad y quien es menor" +
-								"\n3.- Cambiar una cadena a todo mayúsculas y otra a todo minúsculas" +
-								"\n4.- Cargar nuevamente ambos String" +
-								"\n0.- Salir"
-								+ "\nElige opción: ");
-						//Recoger una variable por consola
-						Scanner entrada=new Scanner(System.in);
-						select =entrada.nextInt();
-						//Ejemplo de switch case en Java
-						switch(select){
-						case 1: 
-								System.out.println("Primer String: "+a.toString()+"\nSegundo String: "+b.toString());
-								System.out.println("Largo del Primer String: "+a.length()+"\nLargo del Segundo String: "+b.length());
-								break;
-						case 2: 
-								System.out.println("Sin hacer!!!!");
-								break;
-						case 3: 
-							System.out.println("Primer String a mayuscula: "+a.toUpperCase().toString()+"\n Segundo String: "+b.toLowerCase().toString());
-							
-							break;
-						case 4: 
-							a=cargarString();
-							b=cargarString();
-							break;
-						case 0: 
-							System.out.println("Adios!");
-							break;
-						default:
-							System.out.println("Número no reconocido");break;
-						}
-						
-						System.out.println("\n"); //Mostrar un salto de línea en Java
-						
-					
+			//Complete aquÃ­
 			}
 
 		}
-	
-	
-		
 		
 	}
 	
