edbce5c34799f3b5211a2a0e99d21c825a933577
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2023-12-26 20:07:48 -0300

    Update Principal.java

--- src/ejer2/Principal.java
+++ src/ejer2/Principal.java
@@ -2,7 +2,7 @@ package ejer2;
 
 public class Principal {
 	public static void main(String[] args){
-		Programa miProg2=new Programa();
+		Programa2 miProg2=new Programa2();
 		miProg2.iniciar();
 	}
 }
