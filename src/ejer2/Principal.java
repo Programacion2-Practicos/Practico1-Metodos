0f46184cb9fa73cfb255429802cefa63f14e69f4
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2023-12-27 09:32:46 -0300

    Update Principal.java

--- src/ejer2/Principal.java
+++ src/ejer2/Principal.java
@@ -1,8 +1,20 @@
 package ejer2;
+import java.util.Scanner;
 
 public class Principal {
-	public static void main(String[] args){
-		Programa miProg2=new Programa();
-		miProg2.iniciar();
-	}
+	public static void main(String[] args) {
+		Calculadora calc = new Calculadora();
+		Scanner entrada = new Scanner(System.in);
+				
+		double num1,num2;
+				
+		//pedida de numeros a los usuarios
+		
+		
+		//pasaje de los numeros a los metodos
+		
+		
+		//mostrar en pantalla
+	} 
+
 }
