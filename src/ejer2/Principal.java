19c14ae2ab7117d6b97c5f009c585f10d20b979d
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2023-12-27 09:39:32 -0300

    Update Principal.java

--- src/ejer2/Principal.java
+++ src/ejer2/Principal.java
@@ -9,12 +9,16 @@ public class Principal {
 		double num1,num2;
 				
 		//pedida de numeros a los usuarios
-		
+		System.out.println("Ingrese el primer numero: ");
+		num1 = entrada.nextInt();
+		System.out.println("Ingrese el segundo numero: ");
+		num2 = entrada.nextInt();
 		
 		//pasaje de los numeros a los metodos
-		
+		double result1 = calc.suma(num1,num2);
 		
 		//mostrar en pantalla
+		System.out.println("El resultado de la suma es: " + result1);
 	} 
 
 }
