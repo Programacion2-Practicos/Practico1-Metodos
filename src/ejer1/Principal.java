2e00ad5f2c0a1543954d8b63f52847e7651f70b5
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2023-12-27 09:12:06 -0300

    Update Principal.java

--- src/ejer1/Principal.java
+++ src/ejer1/Principal.java
@@ -3,8 +3,13 @@ package ejer1;
 public class Principal {
 
 	public static void main(String[] args) {
-		Programa ej1=new Programa();
-		ej1.iniciar();
+		Calculadora p=new Calculadora();
+		int a=6, b=3;	
+		double result1= p.suma(a, b);
+		System.out.println("El resultado de la suma es: "+ result1);
+
+		//Complete para probar el resto de los métodos del ejer1
+	}
 	}
 
 }
