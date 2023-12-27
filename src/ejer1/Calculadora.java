0d9251cf129af5d500dd68bdd8739bb6a5ff0f1f
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2023-12-27 09:10:37 -0300

    Update and rename Programa.java to Calculadora.java

---
+++ src/ejer1/Calculadora.java
@@ -0,0 +1,16 @@
+package ejer1;
+
+public class Calculadora {
+	
+	private double sumar(double a, double b) {
+				
+		return (a*b);
+		
+	}
+
+	//complete el resto de los métodos solicitados
+	
+	
+	
+	
+}
